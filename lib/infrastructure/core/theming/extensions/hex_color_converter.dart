25be064e702388734d3cf80b4d07ff16465705ac
Nikita Masev <n.masev@bastion-tech.ru>
2025-07-02 15:13:03 +0300

    fixed deprecated hex_color_converter.dart;

--- lib/infrastructure/core/theming/extensions/hex_color_converter.dart
+++ lib/infrastructure/core/theming/extensions/hex_color_converter.dart
@@ -3,21 +3,22 @@ import 'dart:ui';
 extension HexColor on Color {
   /// String is in the format "aabbcc" or "ffaabbcc" with
   /// an optional leading "#".
-  static Color fromHex(final String hexString) {
-    final buffer = StringBuffer();
-    if (hexString.length == 6 || hexString.length == 7) {
-      buffer.write('ff');
-    }
-    buffer.write(hexString.replaceFirst('#', ''));
-    return Color(int.parse(buffer.toString(), radix: 16));
+  static Color fromHex(final String hex) {
+    final cleaned = hex.replaceFirst('#', '');
+    final argb = cleaned.length == 6 ? 'ff$cleaned' : cleaned;
+    return Color(int.parse(argb, radix: 16));
   }
 
   /// Prefixes a hash sign if [leadingHashSign] is set to `true`
   /// (default is `true`).
-  // ignore: prefer_final_parameters
-  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
-      '${alpha.toRadixString(16).padLeft(2, '0')}'
-      '${red.toRadixString(16).padLeft(2, '0')}'
-      '${green.toRadixString(16).padLeft(2, '0')}'
-      '${blue.toRadixString(16).padLeft(2, '0')}';
+  String toHex({bool leadingHashSign = true}) {
+    int f8(double c) => (c * 255).round() & 0xff; // float->8-бит
+    final a8 = f8(a);
+    final r8 = f8(r);
+    final g8 = f8(g);
+    final b8 = f8(b);
+    final hex =
+        (a8 << 24 | r8 << 16 | g8 << 8 | b8).toRadixString(16).padLeft(8, '0');
+    return leadingHashSign ? '#$hex' : hex;
+  }
 }
